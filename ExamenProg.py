b2be54a5772c04a37f64aa9316de928071f071e6
Pereira <mpereirafernandez@danielcastelao.org>
2025-11-04 14:02:32 +0100

    Comentarios

--- ExamenProg.py
+++ ExamenProg.py
@@ -1,3 +1,4 @@
+#Empezamos ejercicio 1 en el cual tenemos que registrar datos en una tabla a través del teclado.
 #Ejercicio 1
 m = int(input("Introduce a temperatura del día 1 en grados celsius: "))
 n = int(input("Introduce a temperatura del día 2 en grados celsius: "))
@@ -7,33 +8,33 @@ q = int(input("Introduce a temperatura del día 5 en grados celsius: "))
 r = int(input("Introduce a temperatura del día 6 en grados celsius: "))
 s = int(input("Introduce a temperatura del día 7 en grados celsius: "))
 l = (m,n,o,p,q,r,s)
-
+#Empezamos el ejercicio 2 el cual trata de hacer el calculo de la media de la temperatura.
 #Ejercicio 2
 media = sum(l) / 7
 print("La temperatura media es de",media,"grados")
-
+#Empezamos el ejercicio 3 el cual se trata sobre un contador el cual suma 1 dia si la temperatura estuvo por encima de la media.
 #Ejercicio 3
 def calculoDias(contador):
     cantador = 0
     if valor>media:
         contador = contador + 1
     print("El total de días en los que la temperatura estubo por encima de la media fue de",contador)
-print(calculoDias(contador))
 
+#Empezamos el ejercicio 4 el cual pide mostrar por pantalla los dias que la temperatura estubo por encima de la media y mostrar también la temperatura.
 #Ejercicio 4
 for valor in l:
     if valor>media:
         if valor == m:
-            print("El lunes la temperatura estubo por encima de la media ya que ese día hixo",m,"grados")
+            print("El lunes la temperatura estuvo por encima de la media ya que ese día hixo",m,"grados")
         elif valor == n:
-            print("El martes la temperatura estubo por encima de la media ya que ese día fixo",n,"grados")
+            print("El martes la temperatura estuvo por encima de la media ya que ese día fixo",n,"grados")
         elif valor == o:
-            print("El miercoles la temperatura estubo por encima de la media ya que ese día fixo",o,"grados")
+            print("El miercoles la temperatura estuvo por encima de la media ya que ese día fixo",o,"grados")
         elif valor == p:
-            print("El jueves la temperatura estubo por encima de la media ya que ese día fixo",p,"grados")
+            print("El jueves la temperatura estuvo por encima de la media ya que ese día fixo",p,"grados")
         elif valor == q:
-            print("El viernes la temperatura estubo por encima de la media ya que ese día fixo",q,"grados")
+            print("El viernes la temperatura estuvo por encima de la media ya que ese día fixo",q,"grados")
         elif valor == r:
-            print("El sabado la temperatura estubo por encima de la media ya que ese día fixo",r,"grados")
+            print("El sabado la temperatura estuvo por encima de la media ya que ese día fixo",r,"grados")
         elif valor == s:
-            print("El domingo la temperatura estubo por encima de la media ya que ese día fixo",s,"grados")
+            print("El domingo la temperatura estuvo por encima de la media ya que ese día fixo",s,"grados")
